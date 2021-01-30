8361179d51e40bd2ebbef98d099981e3caefffd4
Patrickode <prm5983@rit.edu>
2021-01-30 17:58:26 -0500

    Imported KCG student's work and remade UI. | KCGの学生の作品を輸入してUIをリメイクしました。

---
+++ Memory Doctor/Assets/Scripts/scenechange3.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange3 : MonoBehaviour
+{
+    public void gamestart(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
